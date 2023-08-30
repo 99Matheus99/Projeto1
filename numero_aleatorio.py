75de9d42e64d221f30fcbaebe8bbb9e0d8c45239
Matheus de Oliveira <nubinhopro5+pontape@gmail.com>
2023-08-29 23:13:39 -0300

    adicionei um for para deixar mais interativo

--- numero_aleatorio.py
+++ numero_aleatorio.py
@@ -1,3 +1,7 @@
 from random import randint
+from time import sleep
 valor = input('Digite um número entre 0 e 3: ')
+for c in range(0,3):
+    print('...')
+    sleep(1)
 print(f'Você escolheu o número {valor}, eu escolhi {randint(0, 3)}')
